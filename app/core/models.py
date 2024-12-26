c5bebf56d5c3534b33b58e98f4a0bca269527d12
yehiasalah <yehiasalah74@gmail.com>
2024-12-26 15:49:46 +0200

    solve white space

--- app/core/models.py
+++ app/core/models.py
@@ -20,7 +20,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-    
+
     def create_superuser(self, email, password):
         """Create and return a new superuser."""
         user = self.create_user(email, password)
@@ -29,7 +29,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
-    
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
