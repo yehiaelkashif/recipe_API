ea282fb062b7bc72a80bfd63866e7af3dab9afdb
yehiasalah <yehiasalah74@gmail.com>
2024-12-26 15:46:11 +0200

    line  fix

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     """Manager for users."""
     def create_user(self, email, password=None, **extra_fields):
@@ -19,6 +20,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+    
     def create_superuser(self, email, password):
         """Create and return a new superuser."""
         user = self.create_user(email, password)
@@ -27,6 +29,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+    
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
@@ -34,6 +37,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-
     objects = UserManager()
     USERNAME_FIELD = 'email'
