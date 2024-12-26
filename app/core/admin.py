bc532fe639a93cfa17096ce3498a4da05bffb2a9
yehiasalah <yehiasalah74@gmail.com>
2024-12-26 15:36:12 +0200

    asd

--- app/core/admin.py
+++ app/core/admin.py
@@ -8,6 +8,7 @@ from core import models
 
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
+    
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
--- app/core/models.py
+++ app/core/models.py
@@ -11,6 +11,7 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """Manager for users."""
 
+
     def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a new user."""
         if not email:
@@ -31,6 +32,7 @@ class UserManager(BaseUserManager):
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
+
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
