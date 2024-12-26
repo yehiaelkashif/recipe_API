184746bce7f7bab89c5b1f7d6c51954847fe7c02
yehiasalah <yehiasalah74@gmail.com>
2024-12-26 16:05:30 +0200

    solve lint issues

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -20,10 +20,9 @@ class ModelTests(TestCase):
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
     def test_new_user_email_normalized(self):
+        
         """Test email is normalized for new users."""
-
         sample_emails = [
-
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
             ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
@@ -40,6 +39,7 @@ class ModelTests(TestCase):
             get_user_model().objects.create_user('', 'test123')
     
     def test_create_superuser(self):
+
         """Test creating a superuser."""
         user = get_user_model().objects.create_superuser(
             'test@example.com',
@@ -47,4 +47,3 @@ class ModelTests(TestCase):
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-    
